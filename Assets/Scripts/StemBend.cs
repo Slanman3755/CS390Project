19a184751da1424aec81da4f3b2815074cdf19b8
Justin Hartman <hartma62@purdue.edu>
2019-09-30 23:06:13 -0400

    Testing Bullet Physics

---
+++ Assets/Scripts/StemBend.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StemBend : MonoBehaviour
+{
+    public Vector3 velocity;
+    private Vector3 lastPosition;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = transform.position - lastPosition;
+        lastPosition = transform.position;
+    }
+}
